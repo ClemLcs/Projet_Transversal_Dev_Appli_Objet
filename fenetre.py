b733d2f023bcb13b9637b6d564f14d63afa9e776
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 17:36:22 +0100

    Modification de la classe Fenêtre

--- fenetre.py
+++ fenetre.py
@@ -1,19 +1,23 @@
 from tkinter import *
-from pil import Image, ImageTk
-
 class Fenetre():
     
     __fenetre__ = None
-    __bouton_quitter__ = None
+    __form_arriere_plan__ = None
     __image_epsi__ = None
+    __bouton_quitter__ = None
 
     #Constructeur
     def __init__(self):
 
+        #Configuration de la fenêtre
         self.__fenetre__ = Tk()
+        form_arriere_plan = Canvas(self.__fenetre__,bg='#271549',height=700,width=1000)
 
+        #Ajout du logo Epsi
         self.import_logo_epsi()
+        self.add_logo_epsi(form_arriere_plan)
 
+        #Ajout du bouton quitter
         self.bouton_quitter()
         self.__fenetre__.mainloop()
 
@@ -24,7 +28,14 @@ class Fenetre():
 
     #Fonction qui importe le logo de l'epsi
     def import_logo_epsi(self):
-        self.__image_epsi__ = Image.open()
+        self.__image_epsi__ = PhotoImage(file="media/logo.png")
+ 
+    #Fonction qui ajoute  à l'écran le logo de l'epsi
+    def add_logo_epsi(self, form1):
+        form1.create_image(500, 400, image = self.__image_epsi__)
+        form1.pack()
+
+        
 
 
 
