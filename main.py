3db4664b5b3c34dc657ce6ef23fb10c1f290440f
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-25 10:51:34 +0100

    Modification du main

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import time
 def main():
     root = Tk()
     fenetre = Animation_Logo_EPSI(root)
-    fenetre.after(1)
-    fenetre.after(2)
-    fenetre.after(3)
     root.resizable(width=False, height=False)
     root.mainloop()
 
