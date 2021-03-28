a896d4f1683240135fb5adae6c5e5eecc6222d77
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-28 10:31:18 +0200

    Update case.py

--- case.py
+++ case.py
@@ -1,22 +1,24 @@
-class Case:
+class Case(object):
+    #Constructeur
     def __init__(self,x,y,couleur) :
+        #Chaque case a comme attributs ses coordonées  x et y sa couleur qui est soit noir , soit blanc ou soit vide 
+        #et sa liste coupJouable qui definit  si lorsque l'utilisateur joue sur cette case cela permettra de retourner un pion adverse
+
             self.__x=x
             self.__y=y
             self.__couleur= couleur
-            self.__coupAJouer=[]
-    
-    @property
-    def x(self):
-        return self.coords[0]
+            self.__coupJouable=[]
 
+    #Définition du Getter
     @property
     def x(self):
         return self.__x
     
     @property
-    def coupAJouer(self):
-        return self.__coupAJouer
+    def coupJouable(self):
+        return self.__coupJouable
     
+    #Définition des setters
     @property
     def y(self):
         return self.__y
