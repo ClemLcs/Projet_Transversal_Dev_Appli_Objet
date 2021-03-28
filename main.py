a0dfa5c7535a4746df27dcc1a67d0d0926067dde
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-28 11:56:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from Animation_Introduction import Animation_Introduction
 import time
 import os
+from partie import *  
 
 
 def main():
@@ -9,5 +10,9 @@ def main():
     root = Tk()
     Animation_Introduction(root)
     root.mainloop()
+    
+    lancetapartie = Partie()
+    lancetapartie.quelEstLeTypeDePartie()
+    lancetapartie.laPartie()
 
 main()
