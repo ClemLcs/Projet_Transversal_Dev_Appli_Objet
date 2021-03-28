84600521847bd4ddedd4e846e12f5f7a70b0e1da
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-28 23:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+from partie import *  
 from tkinter import *
 from Animation_Introduction import Animation_Introduction
-import time
-import os
-from partie import *  
-
 
 def main():
 
     root = Tk()
     Animation_Introduction(root)
+    root.after(17000, lambda: root.destroy())
     root.mainloop()
     
-    lancetapartie = Partie()
-    lancetapartie.quelEstLeTypeDePartie()
-    lancetapartie.laPartie()
+    lancetapartie = Partie() # Création de l'objet
+    lancetapartie.quelEstLeTypeDePartie() # Choix des paramètres de partie
+    lancetapartie.laPartie() # Lancement de la partie
 
 main()
