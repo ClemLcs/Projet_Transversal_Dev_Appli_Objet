71d173f886c7c48535f8633e1f55d443747f240c
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-28 23:23:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from partie import *  
-from tkinter import *
-from Animation_Introduction import Animation_Introduction
 
 def main():
+     
+    print("Projet Othello Groupe 7: Clément, Ali, Paul")
 
-    root = Tk()
-    Animation_Introduction(root)
-    root.after(17000, lambda: root.destroy())
-    root.mainloop()
-    
     lancetapartie = Partie() # Création de l'objet
     lancetapartie.quelEstLeTypeDePartie() # Choix des paramètres de partie
     lancetapartie.laPartie() # Lancement de la partie
